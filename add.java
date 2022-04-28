afaa47c3f3e14157c5120189fb709c9dbb8ee007
hirtika <67864747+hirtika@users.noreply.github.com>
2022-04-28 22:24:11 +0530

    Update add.java

--- add.java
+++ add.java
@@ -8,9 +8,7 @@ class add
 			int b = Integer.parseInt(args[1]);
 
 			System.out.println("Addition of two numbers " +a+ " " +b+ "is:" +(a+b));
-			System.out.println("Subtraction of two numbers"  +a + " " +b + "is:" +(a-b));
-			System.out.println("Multiplication of two numbers"  +a + " " +b + "is:" +(a*b));	
-			System.out.println("Division of two numbers"  +a + " " +b + "is:" +(a/b));	
+	
 
 				}
 
